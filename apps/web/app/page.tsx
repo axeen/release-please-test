c5e08cd56ea4b9e19444c8f446c6a05cad132c9a
Jakob Axén Johansson <jakobaxenjohansson@gmail.com>
2024-06-17 19:57:05 +0200

    feat: flare is back

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -63,7 +63,7 @@ export default function Page(): JSX.Element {
       </div>
 
       <Button appName="web" className={styles.button}>
-        please!..........dev!
+        please!..........dev!!!
       </Button>
 
       <div className={styles.hero}>
